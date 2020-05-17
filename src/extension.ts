3b52d7135153037094a718b5951a3a342680af08
pdamianik <39028343+pdamianik@users.noreply.github.com>
2020-05-17 14:38:47 +0200

    The dependencie does it for me

--- src/extension.ts
+++ src/extension.ts
@@ -6,28 +6,6 @@ const EXT_ID = 'pdamianik.zip-archive-type';
 
 export function activate(context: vscode.ExtensionContext) {
 	vscode.extensions.getExtension('pdamianik.folder-archiver')?.exports.registerArchiveType(EXT_ID, zipArchiveType);
-	var activeExtensionIdsArray : string[] = [];
-
-	for (let extension of vscode.extensions.all) {
-		activeExtensionIdsArray.push(extension.id);
-	}
-	
-	vscode.extensions.onDidChange(() => {
-		if (activeExtensionIdsArray.length > vscode.extensions.all.length) {
-			for (let activeExtensionId of activeExtensionIdsArray) {
-				let extension = vscode.extensions.getExtension(activeExtensionId);
-				if (extension === undefined || !extension.isActive) {
-					vscode.extensions.getExtension('pdamianik.folder-archiver')?.exports.registerArchiveType(EXT_ID, zipArchiveType);
-				}
-			}
-		}
-
-		activeExtensionIdsArray = [];
-	
-		for (let extension of vscode.extensions.all) {
-			activeExtensionIdsArray.push(extension.id);
-		}
-	});
 }
 
 export function deactivate() {
